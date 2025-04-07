736bfb3161e8736ea25d44d12d2e1cdfa6609c3e
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-04-07 14:24:57 +0200

    Make memorykind robust against immutable arrays

--- ext/FixedSizeArraysExt.jl
+++ ext/FixedSizeArraysExt.jl
@@ -3,7 +3,7 @@ module FixedSizeArraysExt
 using FixedSizeArrays: FixedSizeArray
 using MemoryViews: MemoryViews, MemoryView, MemoryKind, MutableMemoryView
 
-MemoryViews.MemoryKind(::Type{<:FixedSizeArray{T}}) where {T} = IsMemory(MutableMemoryView{T})
+MemoryViews.MemoryKind(::Type{<:FixedSizeArray{T, N, M}}) where {T, N, M} = MemoryKind(M)
 MemoryViews.MemoryView(x::FixedSizeArray) = MemoryView(x.mem)
 
 end # module
