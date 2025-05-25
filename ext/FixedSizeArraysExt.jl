1bfa7ff05cb5dceaceb44b457ff7ce94f8615464
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-05-25 17:44:30 +0200

    Use parent

--- ext/FixedSizeArraysExt.jl
+++ ext/FixedSizeArraysExt.jl
@@ -4,6 +4,11 @@ using FixedSizeArrays: FixedSizeArray
 using MemoryViews: MemoryViews, MemoryView, MemoryKind, MutableMemoryView
 
 MemoryViews.MemoryKind(::Type{<:FixedSizeArray{T, N, M}}) where {T, N, M} = MemoryKind(M)
-MemoryViews.MemoryView(x::FixedSizeArray) = MemoryView(x.mem)
+
+function MemoryViews.MemoryView(x::FixedSizeArray)
+    mem = MemoryView(parent(x))
+    ind = only(parentindices(x))
+    return @inbounds mem[ind]
+end
 
 end # module
