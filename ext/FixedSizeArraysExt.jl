1b5da89bc63fcb473866e02f5d75c6c288f944b8
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-09-09 13:01:53 +0200

    FixedSizeArrays: Do not access private field

--- ext/FixedSizeArraysExt.jl
+++ ext/FixedSizeArraysExt.jl
@@ -4,6 +4,6 @@ using FixedSizeArrays: FixedSizeArray
 using MemoryViews: MemoryViews, MemoryView, MemoryKind, MutableMemoryView
 
 MemoryViews.MemoryKind(::Type{<:FixedSizeArray{T, N, M}}) where {T, N, M} = MemoryKind(M)
-MemoryViews.MemoryView(x::FixedSizeArray) = MemoryView(x.mem)
+MemoryViews.MemoryView(x::FixedSizeArray) = MemoryView(parent(x))
 
 end # module
