844f9549c70821bb373fbb3e595e2cad0b7e8464
Jakob Nybo Andersen <jakobnybonissen@gmail.com>
2026-09-03 19:57:44 +0200

    Fix LibDeflate tests (#48)
    Tests were not compatible with LibDeflate v1. Fix them to be.
    Eventually, I might want to drop LibDeflate v0.4. However, v1 requires Julia
    v1.12, so this may not be for a couple of years.

--- ext/LibDeflateExt.jl
+++ ext/LibDeflateExt.jl
@@ -4,15 +4,20 @@ using LibDeflate: LibDeflate
 using MemoryViews: MemoryView, MutableMemoryView
 
 # Note: I intentionally limits this to `UInt8` memory, even though the LibDeflate
-# type supports all bitstypes.
+# type supports all bitstypes (on v0.4).
 # I think reading/writing arbitrary bitstypes using pointers is a little shady,
 # so the user should opt-in by constructing the pointers manually.
+#
+# For LibDeflate v1, this is restricted to byte arrays. Users may use the
+# raw pointer methods to opt-into using MemoryView of other eltypes in LibDeflate.
 function LibDeflate.ReadableMemory(mem::MemoryView{UInt8})
-    return LibDeflate.ReadableMemory(pointer(mem), length(mem) % UInt)
+    len = (length(mem) & typemax(Int)) % UInt
+    return LibDeflate.ReadableMemory(pointer(mem), len)
 end
 
 function LibDeflate.WriteableMemory(mem::MutableMemoryView{UInt8})
-    return LibDeflate.WriteableMemory(pointer(mem), length(mem) % UInt)
+    len = (length(mem) & typemax(Int)) % UInt
+    return LibDeflate.WriteableMemory(pointer(mem), len)
 end
 
 end # module
